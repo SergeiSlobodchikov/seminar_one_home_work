3497924a3dc489d1efaa763fb4d08bde8c24cf15
Sergei Slobodchikov <cerhclobodhikow@mail.ru>
2022-12-13 20:28:08 +0300

    commit seminar2

---
+++ functions/functions.py
@@ -0,0 +1,19 @@
+def is_digit(number):
+    try:
+        int(number)
+        return True
+    except ValueError:
+        return False
+
+def Number(text):
+    number = input(f'{text}')
+    while(not is_digit(number)):
+        number = input('ОШИБКА. Попробуйте снова: ')
+    return int(number)
+
+
+def is_natural(text):
+    number = input(f'{text}')
+    while(not number.isdigit()):
+        number = input('ОШИБКА. Попробуйте снова: ')
+    return int(number)
