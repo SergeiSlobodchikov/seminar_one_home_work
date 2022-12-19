6fdec6fe46d5fd7a2442aedad20c07c2a97b9ce5
Sergei Slobodchikov <cerhclobodhikow@mail.ru>
2022-12-20 00:54:03 +0300

    commit seminar3 Home Work

---
+++ seminar_3/HomeWorkSeminar3/seminar3_2HW.py
@@ -0,0 +1,47 @@
+# Задача 18:
+# Требуется найти в массиве A[1..N] самый близкий по величине элемент к заданному числу X.
+# Пользователь вводит натуральное число N – количество элементов в массиве и число, которое необходимо проверить - X.
+# Заполните массив случайными натуральными числами от 1 до N/2.
+# Выведите, ближайший к X элемент. Если есть несколько элементов, которые равноудалены от X, выведите наименьший.
+# Ввод: 5
+# Ввод: 6
+# 1 2 0 4 7
+# Вывод: 7
+import random
+
+n = int(input('Введите длину массива: '))
+x = int(input(f'Введите число, которое необходимо проверить '))
+massiveA = []
+for i in range(n):
+    random_number = round(random.randint(1, n*2))
+    massiveA.append(random_number)
+print(massiveA)
+
+condition = True
+min = x-1
+max = x+1
+off = 0
+while condition: 
+    if off == 0:
+        for i in massiveA:
+            if i == x:
+                print(f'Ну то же самое число самое близкое')
+                condition = False
+                break
+        else:
+            off = 1
+            
+    if off == 1:
+        for i in massiveA:
+            if i == min or i == max: 
+                if i == min:
+                    print(f'Самое близкое число {min}')
+                else:
+                    print(f'Самое близкое число {max}')
+                condition = False
+                break
+        min -= 1
+        max += 1
+
+
+
---
+++ seminar_3/seminar3.py
@@ -0,0 +1,110 @@
+# 17
+# Дан список чисел. Определите, сколько в нем встречается различных чисел.
+# Input: [1, 1, 2, 0, -1, 3, 4, 4]
+# Output: 6
+# import random
+
+# n = int(input('Введите длину списка >>> '))
+# l = []
+# for num in range(0,n):
+    
+#     random_number = round(random.randint(0,5))
+#     l.append(random_number)
+
+# a = set(l)
+
+# print(l)
+# print(len(a))
+
+
+
+# 19
+# Дана последовательность из N целых чисел и число K. 
+# Необходимо сдвинуть всю последовательность (сдвиг - циклический) на K элементов вправо,  K – положительное число.
+# Input:   [1, 2, 3, 4, 5] k = 2
+# Output:  [4, 5, 1, 2, 3]
+
+# import random
+
+# n = int(input('Введите длину списка >>> '))
+# l = []
+# for num in range(0,n):
+    
+#     random_number = round(random.randint(-10,10))
+#     l.append(random_number)
+# print(l)
+
+# k = int(input('Введите на сколько индексов сдвигать >>> '))
+# for i in range(k):
+#     p = l.pop(-1)
+#     l.insert(0, p)
+
+# print(l)
+
+
+# Напишите программу для печати всех уникальных значений в словаре. 
+# Input:  [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"}, {"VII": " S005 "}, {" V ":" S009 "}, {" VIII ":" S007 "}] 
+# Output: {'S005', 'S002', 'S007', 'S001', 'S009'}
+
+# my_dict = {"V": "S001", "V": "S002", "VI": "S001", "VI": "S005", "VII": " S005 ", " V ":" S009 ", " VIII ":" S007 "}
+
+# print(my_dict)
+
+# 23
+# Дан массив, состоящий из целых чисел. Напишите программу, которая подсчитает количество элементов массива, больших предыдущего (элемента с предыдущим номером) 
+
+
+# Input: [0, -1, 5, 2, 3]
+# Output: 2 
+
+# пояснение
+# (-1 < 5, 2 < 3)
+
+import random
+
+n=int(input('Введите длину списка: '))
+list = []
+for i in range(0,n):
+    random_number = round(random.randint(0,50))
+    list.append(random_number)
+print(list)
+
+count = 0
+for i in range(1, len(list)):
+    if list[i-1] < list[i]:
+        count += 1
+
+print(count)
+
+
+import random
+
+l = []
+for num in range(0,10):
+    random_number = round(random.randint(0,5))
+    l.append(random_number)
+
+print(l)
+
+count = 0
+
+for i in range(1, len(l)):
+    if l[i-1]<l[i]:
+        count+=1
+
+print(count)
+dictionary = \
+    {
+#ключ : значение
+"I": "S001",
+"II": "S002",
+"III": "S001",
+"IV": "S005",
+"V": "S006",
+"VI":"S006",
+"VII":"S007"
+    }
+print(dictionary)
+print()
+
+print(set(dictionary.values()))
