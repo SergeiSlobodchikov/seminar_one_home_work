82943c1cb3274642110b3c2ff4f4a1dc69a48556
Sergei Slobodchikov <cerhclobodhikow@mail.ru>
2022-12-20 08:52:53 +0300

    commit seminar3

--- seminar_3/seminar3.py
+++ seminar_3/seminar3.py
@@ -2,19 +2,20 @@
 # Дан список чисел. Определите, сколько в нем встречается различных чисел.
 # Input: [1, 1, 2, 0, -1, 3, 4, 4]
 # Output: 6
-# import random
+import random
 
-# n = int(input('Введите длину списка >>> '))
-# l = []
-# for num in range(0,n):
+
+n = int(input('Введите длину списка >>> '))
+l = []
+for num in range(0,n):
     
-#     random_number = round(random.randint(0,5))
-#     l.append(random_number)
+    random_number = round(random.randint(0,5))
+    l.append(random_number)
 
-# a = set(l)
+a = set(l)
 
-# print(l)
-# print(len(a))
+print(l)
+print(len(a))
 
 
 
@@ -24,36 +25,29 @@
 # Input:   [1, 2, 3, 4, 5] k = 2
 # Output:  [4, 5, 1, 2, 3]
 
-# import random
 
-# n = int(input('Введите длину списка >>> '))
-# l = []
-# for num in range(0,n):
+
+n = int(input('Введите длину списка >>> '))
+l = []
+for num in range(0,n):
     
-#     random_number = round(random.randint(-10,10))
-#     l.append(random_number)
-# print(l)
+    random_number = round(random.randint(-10,10))
+    l.append(random_number)
+print(l)
 
-# k = int(input('Введите на сколько индексов сдвигать >>> '))
-# for i in range(k):
-#     p = l.pop(-1)
-#     l.insert(0, p)
+k = int(input('Введите на сколько индексов сдвигать >>> '))
+for i in range(k):
+    p = l.pop(-1)
+    l.insert(0, p)
 
-# print(l)
+print(l)
 
 
-# Напишите программу для печати всех уникальных значений в словаре. 
-# Input:  [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"}, {"VII": " S005 "}, {" V ":" S009 "}, {" VIII ":" S007 "}] 
-# Output: {'S005', 'S002', 'S007', 'S001', 'S009'}
 
-# my_dict = {"V": "S001", "V": "S002", "VI": "S001", "VI": "S005", "VII": " S005 ", " V ":" S009 ", " VIII ":" S007 "}
-
-# print(my_dict)
 
 # 23
-# Дан массив, состоящий из целых чисел. Напишите программу, которая подсчитает количество элементов массива, больших предыдущего (элемента с предыдущим номером) 
-
-
+# Дан массив, состоящий из целых чисел. Напишите программу, которая подсчитает 
+# количество элементов массива, больших предыдущего (элемента с предыдущим номером) 
 # Input: [0, -1, 5, 2, 3]
 # Output: 2 
 
@@ -77,22 +71,10 @@ for i in range(1, len(list)):
 print(count)
 
 
-import random
+# Напишите программу для печати всех уникальных значений в словаре. 
+# Input:  [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"}, {"VII": " S005 "}, {" V ":" S009 "}, {" VIII ":" S007 "}] 
+# Output: {'S005', 'S002', 'S007', 'S001', 'S009'}
 
-l = []
-for num in range(0,10):
-    random_number = round(random.randint(0,5))
-    l.append(random_number)
-
-print(l)
-
-count = 0
-
-for i in range(1, len(l)):
-    if l[i-1]<l[i]:
-        count+=1
-
-print(count)
 dictionary = \
     {
 #ключ : значение
