7df6c3fd2efde59205e19d5582a0105d9760255f
Sergei Slobodchikov <cerhclobodhikow@mail.ru>
2023-01-24 20:04:14 +0300

    Калькулятор минусы

--- seminar_8/code_calculator.py
+++ seminar_8/code_calculator.py
@@ -24,11 +24,14 @@
 # (12 - 4) * 2
 
 def split_string(stroka: str):
-  string = stroka.replace(' ', '').replace('+', ' + ').replace('-', ' - ').replace('*', ' * ').replace('/', ' / ').replace('(', '( ').replace(')', ' )')
+  string = stroka.replace(' ', '').replace('+', ' + ').replace('*-', ' * -').replace('/-', ' / -').replace('*', ' * ').replace('/', ' / ').replace('(', '( ').replace(')', ' )')
+  string = string.replace('1-', '1 + -').replace('2-', '2 + -').replace('3-', '3 + -').replace('4-', '4 + -').replace('5-', '5 + -')
+  string = string.replace('6-', '6 + -').replace('7-', '7 + -').replace('8-', '8 + -').replace('9-', '9 + -').replace('0-', '0 + -')
   my_list = string.split()
   if my_list[0] == '-':
     my_list[0] = float(my_list[1])*(-1)
     my_list.pop(1)
+  print(my_list)
   return my_list
 
 def calculator(my_list: list):
